5d538d557869c71b604eaeeb4fbf946a52a1af7d
Olav Valle <olavval@stud.ntnu.no>
2021-04-04 22:44:14 +0200

    implemented first draft of main view Consists of a header bar and add/edit article FAB

--- src/App.js
+++ src/App.js
@@ -1,9 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    AppBar,
+    Button,
+    Container, Fab, fade,
+    IconButton, InputAdornment, InputBase,
+    makeStyles,
+    MuiThemeProvider,
+    TextField,
+    Toolbar, Typography
+} from '@material-ui/core'
+import {Menu, PostAdd} from '@material-ui/icons'
+import theme from "./theme"
+import {SearchBox} from "./SearchBox";
+
+
+const useStyles = makeStyles((theme) => ({
+        main: {
+            color: "white",
+            justify: "space-around",
+            // justifyContent: "center",
+            // alignContent: "center"
+        },
+        homeButton: {
+            color: "white",
+            padding: theme.spacing(2),
+        },
+        actionButton: {
+            position: "absolute",
+            bottom: theme.spacing(2),
+            right: theme.spacing(2),
+        },
+        title: {
+            justifyContent: "center",
+            flexGrow: 1,
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+                display: 'block',
+            }
+
+        },
+        menuButton: {
+            marginRight: theme.spacing(2)
+        },
+
+    })
+)
 
 function App() {
-  return null;
+    const classes = useStyles();
+
+    return (
+        // todo: refactor header to separate comp
+        // todo: add a home/main button
+        // todo: add a "New Post" button
+
+        <MuiThemeProvider theme={theme}>
+            <Container>
+                <AppBar className={classes.main} position={"static"}>
+                    <Toolbar>
+                        <IconButton
+                            edge="start"
+                            className={classes.menuButton}
+                        >
+                            <Menu/>
+                        </IconButton>
+
+
+                        <Typography className={classes.title} variant={"h5"}>
+                            #blog
+                        </Typography>
+
+                        <Button className={classes.homeButton}>Home</Button>
+                        <SearchBox/>
+                    </Toolbar>
+                </AppBar>
+                <Fab className={classes.actionButton} color="primary" aria-label="add">
+                    <PostAdd />
+                </Fab>
+            </Container>
+        </MuiThemeProvider>
+    )
 }
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core";
+import {blue, green, orange, purple, red} from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+    palette: {
+
+    },
+    status: {
+    },
+})
+
+export default theme;
